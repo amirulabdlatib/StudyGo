f5f997bba819a357da4050b766261bfcbc5a1cfe
Amirul <amirulal73@gmail.com>
2023-06-03 21:31:50 +0800

    my_project_visualized.png

--- StudyGo/StudyGo/settings.py
+++ StudyGo/StudyGo/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'StudyApp',
+    'django_extensions'
 ]
 
 MIDDLEWARE = [
@@ -130,3 +131,8 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'StudyApp.CustomUser'
+
+GRAPH_MODELS = {
+  'all_applications': True,
+  'group_models': True,
+}
